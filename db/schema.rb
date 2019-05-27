bc8d43e5006066d4b1bfc34db8e96fe508b64f5f
Margaux FESLARD margaux.feslard@gmail.com <margaux.feslard@gmail.com>
2019-05-27 16:51:15 +0200

    pool created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_144434) do
+ActiveRecord::Schema.define(version: 2019_05_27_144949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pools", force: :cascade do |t|
+    t.string "address"
+    t.text "description"
+    t.integer "price_per_day"
+    t.integer "capacity"
+    t.string "picture"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pools_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +41,5 @@ ActiveRecord::Schema.define(version: 2019_05_27_144434) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pools", "users"
 end
