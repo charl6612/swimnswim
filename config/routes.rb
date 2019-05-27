dd8e44b5dce51ff001377477573a26ac2c9d6f76
charles noppe <noppe.charles@gmail.com>
2019-05-27 17:02:49 +0200

    solve routes new

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
+  resources :pools
   get 'rentals/new'
   get 'rentals/create'
   get 'rentals/update'
   get 'rentals/destroy'
-  get 'pools/search'
-  get 'pools/index'
-  get 'pools/new'
-  get 'pools/create'
-  get 'pools/show'
-  get 'pools/edit'
-  get 'pools/update'
-  get 'pools/destroy'
+
   get 'users/dashboard'
   get 'users/edit'
   get 'users/update'
