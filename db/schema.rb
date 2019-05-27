a32f2b22ed5c4cfa1bb00f3dc1d768ca47d81ee7
Margaux FESLARD margaux.feslard@gmail.com <margaux.feslard@gmail.com>
2019-05-27 16:54:35 +0200

    review created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_145227) do
+ActiveRecord::Schema.define(version: 2019_05_27_145348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,17 @@ ActiveRecord::Schema.define(version: 2019_05_27_145227) do
     t.index ["user_id"], name: "index_rentals_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "description"
+    t.integer "rating"
+    t.bigint "pool_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pool_id"], name: "index_reviews_on_pool_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -56,4 +67,6 @@ ActiveRecord::Schema.define(version: 2019_05_27_145227) do
   add_foreign_key "pools", "users"
   add_foreign_key "rentals", "pools"
   add_foreign_key "rentals", "users"
+  add_foreign_key "reviews", "pools"
+  add_foreign_key "reviews", "users"
 end
