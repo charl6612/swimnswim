6b6aea93b234206b6ccfae47a439d4ed3a227492
charles noppe <noppe.charles@gmail.com>
2019-05-28 18:28:36 +0200

    policy rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,11 +2,12 @@ class RentalsController < ApplicationController
   def new
     @pool = Pool.find(params[:pool_id])
     @rental = Rental.new
-
+    authorize @rental
   end
 
   def create        # POST /pools
     @rental = Rental.new(rental_params)
+    authorize @rental
     @rental.pool = Pool.find(params[:pool_id])
     @pool = @rental.pool
     @rental.user = current_user
