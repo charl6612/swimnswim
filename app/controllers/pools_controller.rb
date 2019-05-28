13e0941e1b20bac6abe4aa7e557a3f01de168a8d
Margaux FESLARD margaux.feslard@gmail.com <margaux.feslard@gmail.com>
2019-05-28 17:38:01 +0200

    push

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -16,8 +16,8 @@ class PoolsController < ApplicationController
 
   def create        # POST /pools
     @pool = Pool.new(pool_params)
-    authorize @pool
     @pool.user = current_user
+    authorize @pool
     if @pool.save
       redirect_to pool_path(@pool)
     else
@@ -48,7 +48,6 @@ class PoolsController < ApplicationController
     params.require(:pool).permit(:name, :address, :description, :price_per_day, :capacity, :picture)
   end
 end
-
 
 # t.string "address"
 # t.text "description"
