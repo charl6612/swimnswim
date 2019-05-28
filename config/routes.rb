02c9a06b187311a745d4d7428b83bfcebda41233
charles noppe <noppe.charles@gmail.com>
2019-05-28 11:23:52 +0200

    new reservation

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'users/dashboard'
   get 'users/edit'
   get 'users/update'
- resources :pools
- resources :rentals
+ resources :pools do
+  resources :rentals, only: [ :index, :new, :create ]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
