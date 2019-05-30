6b11addf1842a32183b1bcca5bc4b10fd2984827
Marie Caulliez <mariecaulliez@gmail.com>
2019-05-30 12:45:30 +0200

    Adds avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :store_user_location!, if: :storable_location?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -11,24 +12,29 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || super
   end
-end
 
-private
-# Its important that the location is NOT stored if:
-# - The request method is not GET (non idempotent)
-# - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
-#    infinite redirect loop.
-# - The request is an Ajax request as this can lead to very unexpected behaviour.
-def storable_location?
-  request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
-end
+  private
 
-def store_user_location!
-  # :user is the scope we are authenticating
-  store_location_for(:user, request.fullpath)
+  # Its important that the location is NOT stored if:
+  # - The request method is not GET (non idempotent)
+  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
+  #    infinite redirect loop.
+  # - The request is an Ajax request as this can lead to very unexpected behaviour.
 
-def skip_pundit?
-  devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-end
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
 
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :pools
   has_many :rentals
   has_many :reviews
+  mount_uploader :avatar, PhotoUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
