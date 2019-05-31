90b254312920295f64c8a07453979900d4a6944d
charles noppe <noppe.charles@gmail.com>
2019-05-31 17:00:37 +0200

    fix jours

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -5,6 +5,7 @@ import { autocomplete } from '../components/autocomplete';
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
   if (markers.length === 0) {
